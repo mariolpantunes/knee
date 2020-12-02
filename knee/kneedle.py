31cd7cd32a8cb21d05e914718382ff15fe4d6334
Mário Antunes <mario.antunes@ua.pt>
2020-12-02 18:52:30 +0000

    Added initial code drop for Kneedle and RDP

---
+++ knee/kneedle.py
@@ -0,0 +1,124 @@
+# coding: utf-8
+
+__author__ = 'Mário Antunes'
+__version__ = '0.1'
+__email__ = 'mariolpantunes@gmail.com'
+__status__ = 'Development'
+
+import math
+import numpy as np
+from uts import ema
+from enum import Enum
+#from rdp import rdp
+
+
+class Direction(Enum):
+    Increasing=0
+    Decreasing=1
+
+
+class Concavity(Enum):
+    Counterclockwise=0
+    Clockwise=1
+
+
+def differences(points, cd, cc):
+    rv = np.empty(points.shape)
+    
+    if cd is Direction.Decreasing and cc is Concavity.Clockwise:
+        for i in range(0, len(points)):
+            rv[i][0] = points[i][0]
+            rv[i][1] = points[i][0] + points[i][1] # x + y
+    elif cd is Direction.Decreasing and cc is Concavity.Counterclockwise:
+        for i in range(0, len(points)):
+            rv[i][0] = points[i][0]
+            rv[i][1] = 1.0 - (points[i][0] + points[i][1]) # 1.0 - (x + y)
+    elif cd is Direction.Increasing and cc is Concavity.Clockwise:
+        for i in range(0, len(points)):
+            rv[i][0] = points[i][0]
+            rv[i][1] = points[i][1] - points[i][0] # y - x
+    else:
+        for i in range(0, len(points)):
+            rv[i][0] = points[i][0]
+            rv[i][1] = math.fabs(points[i][1] - points[i][0]) # abs(y - x)
+    
+    return rv
+
+
+def knee(points, sensitivity = 1.0, cd=Direction.Decreasing, cc=Concavity.Clockwise):
+    Ds = ema.linear(points, 1.0)
+    print(Ds)
+
+    pmin = Ds.min(axis = 0)
+    pmax = Ds.max(axis = 0)
+    Dn = (Ds - pmin)/(pmax - pmin)
+    print(Dn)
+
+    Dd = differences(Dn, cd, cc)
+    print(Dd)
+
+    idx = []
+    lmxThresholds = []
+    detectKneeForLastLmx = False
+    knees=[]
+
+    for i in range(1, len(Dd)-1):
+        y0 = Dd[i-1][1]
+        y = Dd[i][1]
+        y1 = Dd[i+1][1]
+        
+        if y0 < y and y > y1:
+            idx.append(i)
+            tlmx = y - sensitivity / (len(Dd) - 1);
+            lmxThresholds.append(tlmx)
+            detectKneeForLastLmx = True
+        
+        if detectKneeForLastLmx:
+            if y1 < lmxThresholds[-1]:
+                knees.append(idx[-1])
+                detectKneeForLastLmx = False
+        
+
+    return np.array(knees)
+
+def auto_knee(points, sensitivity=1.0):
+    start = points[0]
+    end = points[-1]
+
+    m = (end[1] - start[1]) / (end[0] - start[0])
+    b = start[1] - (m * start[0])
+
+    if m > 0.0:
+        cd = Direction.Increasing
+    else:
+        cd = Direction.Decreasing
+    
+    y = np.transpose(points)[1]
+    yhat = np.empty(len(points))
+    for i in range(0, len(points)):
+        yhat[i] = points[i][0]*m+b
+
+    vote = np.sum(y - yhat)
+
+    if cd is Direction.Increasing and vote > 0:
+        cc = Concavity.Clockwise
+    elif cd is Direction.Increasing and vote <= 0:
+        cc = Concavity.Counterclockwise
+    elif cd is Direction.Decreasing and vote > 0:
+        cc = Concavity.Clockwise
+    else:
+        cc = Concavity.Counterclockwise
+    
+    return knee(points, sensitivity, cd, cc)
+
+#points = np.array([[0.0, 0.0], [1.0, 2.0], [1.2, 4.0], [2.3, 6], [2.9, 8], [5, 10]])
+
+l = [[1.0, 1.0],[2, 0.25],[3, 0.111],[4, 0.0625],[5, 0.04],[6, 0.0277777],[7, 0.0204],[8, 0.015625],[9, 0.012345679],[10, .01]]
+points = np.array(l)
+#print(points)
+
+knees = knee(points, 1.0, Direction.Decreasing, Concavity.Counterclockwise)
+print("Knee:", knees)
+
+knees = auto_knee(points)
+print("Auto Knee:", knees)
---
+++ knee/rdp.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+
+__author__ = 'Mário Antunes'
+__version__ = '0.1'
+__email__ = 'mariolpantunes@gmail.com'
+__status__ = 'Development'
+
+
+import numpy as np
+import math
+
+
+def perpendicular_distance(pt, start, end):
+    d = end - start
+
+    mag = math.hypot(d[0], d[1])
+    if mag > 0.0:
+        d /= mag
+    
+    pv = pt - start
+    pvdot = np.dot(d, pv)
+
+    a = pv - pvdot * d
+
+    return math.hypot(a[0], a[1])
+
+def rdp(points, r=0.95):
+    dmax = 0
+    index = 0
+    end = len(points) - 1
+
+    for i in range(1, end):
+        d = perpendicular_distance(points[i], points[0], points[end])
+        if d > dmax :
+            index = i
+            dmax = d
+
+    m = (points[end][1]-points[0][1])/(points[end][0]-points[0][0])
+    b = points[0][1]- (m * points[0][0])
+    y = np.transpose(points)[1]
+    yhat = np.empty(len(y))
+    for i in range(0, end+1):
+        yhat[i] = points[i][0]*m+b
+    
+    ybar = np.sum(y)/len(y)          
+    ssreg = np.sum((y-yhat)**2)
+    sstot = np.sum((y - ybar)**2)
+
+    determination = 1.0 - (ssreg / sstot)
+
+    if determination < r :
+        print("hey")
+        left = rdp(points[0:index+1], r)
+        right = rdp(points[index:end+1], r)
+        return np.concatenate((left[0:len(left)-1], right))
+    else:
+        rv = np.empty([2,2])
+        rv[0] = points[0]
+        rv[1] = points[end]
+        return rv
+
+
+#points = np.array([[0.0, 0.0], [1.0, 2.0], [1.2, 4.0], [2.3, 6], [2.9, 8], [5, 10]])
+#sp = rdp(points)
+#print(sp)
