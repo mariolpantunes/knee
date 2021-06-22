15c48c2c45f89b7a8b7deb08e08d3974632268ca
Mário Antunes <mariolpantunes@gmail.com>
2021-06-22 21:13:32 +0100

    Added docstring to curvature method

--- knee/curvature.py
+++ knee/curvature.py
@@ -8,22 +8,28 @@ __status__ = 'Development'
 
 import logging
 import numpy as np
-from uts import ema
 import knee.multi_knee as mk
-from knee.linear_fit import linear_fit, linear_r2
 
 
 logger = logging.getLogger(__name__)
 
 
-def get_knee_points(points):
-    x = points[:,0]
-    y = points[:,1]
+def knee(points: np.ndarray) -> int:
+    """Returns the index of the knee point based on the curvature equations:
+    $$
+    k = \\frac{|f''(x)|}{(1+[f'(2)]^2)^{\\frac{3}{2}}}
+    $$
 
-    return get_knee(x, y)
+    Args:
+        points (np.ndarray): numpy array with the points (x, y)
 
+    Returns:
+        int: the index of the knee point
+    """
 
-def get_knee(x, y):
+    x = points[:, 0]
+    y = points[:, 1]
+
     gradient1 = np.gradient(y, x, edge_order=1)
     gradient2 = np.gradient(y, x, edge_order=2)
 
@@ -31,65 +37,18 @@ def get_knee(x, y):
     return np.argmax(curvature[0:-2])
 
 
-def multiknee_rec(x, y, left, right, t, debug):
-    logger.info('MultiKnee [%s, %s]', left, right)
-    response = get_knee(x[left:right], y[left:right])
-    knee_idx = response + left
-    response = knee_idx
+def multi_knee(points: np.ndarray, t1: float = 0.99, t2: int = 3) -> np.ndarray:
+    """Recursive knee point detection based on the curvature equations.
 
-    if (knee_idx+1-left) > 4:
-        
-        coef_left = linear_fit(x[left:knee_idx+1], y[left:knee_idx+1])
-        r2_left = linear_r2(x[left:knee_idx+1], y[left:knee_idx+1], coef_left)
-        if r2_left <= t:
-            responce_left = multiknee_rec(x, y, left, knee_idx, t, debug)
-            rv = responce_left
-            rv.append(response)
-        else:
-            rv = [response]
-    else:
-        rv = [response]
+    It returns the knee points on the curve.
 
-    #logger.info('R2[%s, %s] Left Part [%s, %s] Right Part [%s, %s]', r2_left, r2_right, left, knee_idx+1, knee_idx+1, right)
+    Args:
+        points (np.ndarray): numpy array with the points (x, y)
+        t1 (float): coefficient of determination threshold (default 0.99)
+        t2 (int): number of points threshold (default 3)
 
-    #if r2_left < 0:
-    #    logger.error('Coef %s Points [%s, %s]', coef_left, (x[left], y[left]),(x[knee_idx], y[knee_idx]))
-    #    logger.error('%s', x[left:knee_idx+1])
-    #    logger.error('%s', y[left:knee_idx+1])
+    Returns:
+        np.ndarray: The knee points on the curve
 
-
-    if (right-(knee_idx+1)) > 4:
-        coef_right = linear_fit(x[knee_idx+1:right], y[knee_idx+1:right])
-        r2_right = linear_r2(x[knee_idx+1:right], y[knee_idx+1:right], coef_right)
-
-        if r2_right <= t:
-            response_right = multiknee_rec(x, y, knee_idx+1, right, t, debug)
-            rv.extend(response_right)
-    return rv
-
-
-def multiknee(points, t = 0.99, debug=False):
-    Ds = ema.linear(points, 1.0)
-    pmin = Ds.min(axis = 0)
-    pmax = Ds.max(axis = 0)
-    Dn = (Ds - pmin)/(pmax - pmin)
-
-    x = Dn[:,0]
-    y = Dn[:,1]
-
-    coef = linear_fit(x, y)
-    if linear_r2(x, y, coef) <= t:
-        response = multiknee_rec(x,y, 0, len(points), t, debug)
-        if debug:
-            return {'knees': response, 'Ds':Ds, 'Dn': Dn}
-        else:
-            return np.array(response)
-    else:
-        if debug:
-            return {'knees': np.array([]), 'Ds':Ds, 'Dn': Dn}
-        else:
-            np.array([])
-
-
-def multi_knee(points, t1=0.99, t2=4):
-    return mk.multi_knee(get_knee_points, points, t1, t2)
+    """
+    return mk.multi_knee(knee, points, t1, t2)
---
+++ test/test_curvature.py
@@ -0,0 +1,27 @@
+import unittest
+import numpy as np
+
+
+import knee.curvature as curvature
+
+
+class TestL_Method(unittest.TestCase):
+    def test_get_knee(self):
+        x = np.array([0,1,2,3,4,5,6,7,8,9,])
+        y = np.array([1,0.5,0.333333333,0.25,0.2,0.166666667,0.142857143,0.125,0.111111111,0.1])
+        points = np.stack((x, y), axis=1)
+        result = curvature.knee(points)
+        desired = 7
+        self.assertEqual(result, desired)
+    
+    def test_multi_knee(self):
+        x = np.array([0,1,2,3,4,5,6,7,8,9,])
+        y = np.array([1,0.5,0.333333333,0.25,0.2,0.2,0.1,0.06666666667,0.05,0.04])
+        points = np.stack((x, y), axis=1)
+        result = curvature.multi_knee(points, t1=0.9)
+        desired = np.array([2, 4, 7])
+        np.testing.assert_array_equal(result, desired)
+
+
+if __name__ == '__main__':
+    unittest.main()
