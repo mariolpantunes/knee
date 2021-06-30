9e3c99c7c68382370c12f4df5e9fb6ea7e89d6a6
Mario Antunes <mariolpantunes@gmail.com>
2021-06-30 19:13:09 +0100

    Added docstring to optimization module

--- knee/optimization.py
+++ knee/optimization.py
@@ -7,6 +7,7 @@ __status__ = 'Development'
 
 
 import math
+import typing
 import logging
 import numpy as np
 
@@ -14,8 +15,19 @@ import numpy as np
 logger = logging.getLogger(__name__)
 
 
-# hill climbing local search algorithm
-def hillclimbing(objective, bounds, n_iterations=200, step_size=.01):
+def hillclimbing(objective:typing.Callable, bounds:np.ndarray, n_iterations:int=200, step_size:float=.01) -> list:
+    """
+    Hill climbing local search algorithm.
+
+    Args:
+        objective (typing.Callable): objective fucntion
+        bounds (np.ndarray): the bounds of valid solutions
+        n_iterations (int): the number of iterations (default 200)
+        step_size (float): the step size (default 0.01)
+
+    Returns:
+        list: [solution, solution_cost]
+    """
     # min and max for each bound
     bounds_max = bounds.max(axis = 1)
     bounds_min = bounds.min(axis = 1)
@@ -45,9 +57,21 @@ def hillclimbing(objective, bounds, n_iterations=200, step_size=.01):
             #logger.info('>%d f(%s) = %.5f', i, solution, solution_cost)
     return [solution, solution_cost]
 
-	
-# simulated annealing algorithm
-def simulated_annealing(objective, bounds, n_iterations=200, step_size=0.01, temp=20):
+
+def simulated_annealing(objective:typing.Callable, bounds:np.ndarray, n_iterations:int=200, step_size:float=0.01, temp:float=20.0) -> list:
+    """
+    Simulated annealing algorithm.
+
+    Args:
+        objective (typing.Callable): objective fucntion
+        bounds (np.ndarray): the bounds of valid solutions
+        n_iterations (int): the number of iterations (default 200)
+        step_size (float): the step size (default 0.01)
+        temp (float): initial temperature (default 20.0)
+
+    Returns:
+        list: [solution, solution_cost]
+    """
 	# min and max for each bound
     bounds_max = bounds.max(axis = 1)
     bounds_min = bounds.min(axis = 1)
@@ -86,7 +110,16 @@ def simulated_annealing(objective, bounds, n_iterations=200, step_size=0.01, tem
     return [best, best_eval]
 
 
-def get_random_solution(bounds):
+def get_random_solution(bounds:np.ndarray) -> np.ndarray:
+    """
+    Generates a random solutions that is within the bounds.
+
+    Args:
+        bounds (np.ndarray): the bounds of valid solutions
+
+    Returns:
+        np.ndarray: a random solutions that is within the bounds
+    """
     solution = bounds[:, 0] + np.random.rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
     solution = np.minimum(solution, bounds.max(axis = 1))
     solution = np.maximum(solution, bounds.min(axis = 1))
@@ -94,33 +127,52 @@ def get_random_solution(bounds):
 
 
 # genetic algorithm
-def genetic_algorithm(objective, bounds, selection, crossover, mutation, n_iter=100, n_pop=10, r_cross=0.9, r_mut=0.2):
+def genetic_algorithm(objective:typing.Callable, bounds:np.ndarray,
+selection:typing.Callable, crossover:typing.Callable, mutation:typing.Callable,
+n_iter:int=100, n_pop:int=10, r_cross:float=0.9, r_mut:float=0.2) -> list:
+    """
+    Genetic optimization algorithm.
+
+    Args:
+        objective (typing.Callable): objective fucntion
+        bounds (np.ndarray): the bounds of valid solutions
+        selection (typing.Callable): selection fucntion
+        crossover (typing.Callable): crossover fucntion
+        mutation (typing.Callable): mutation fucntion
+        n_iter (int): the number of iterations (default 100)
+        n_pop (int): the number of elements in the population (default 10)
+        r_cross (float): ratio of crossover (default 0.9)
+        r_mut (float): ratio of mutation (default 0.2)
+
+    Returns:
+        list: [solution, solution_cost]
+    """
     # initial population of random bitstring
-	pop = [get_random_solution(bounds) for _ in range(n_pop)]
+    pop = [get_random_solution(bounds) for _ in range(n_pop)]
 	# keep track of best solution
-	best, best_eval = 0, objective(pop[0])
+    best, best_eval = 0, objective(pop[0])
 	# enumerate generations
-	for _ in range(n_iter):
-		# evaluate all candidates in the population
-		scores = [objective(c) for c in pop]
-		# check for new best solution
-		for i in range(n_pop):
-			if scores[i] < best_eval:
-				best, best_eval = pop[i], scores[i]
-				#logger.info('>%d, new best f(%s) = %.3f' % (gen,  pop[i], scores[i]))
-		# select parents
-		selected = [selection(pop, scores) for _ in range(n_pop)]
-		# create the next generation
-		children = list()
-		for i in range(0, n_pop, 2):
-			# get selected parents in pairs
-			p1, p2 = selected[i], selected[i+1]
-			# crossover and mutation
-			for c in crossover(p1, p2, r_cross):
-				# mutation
-				mutation(c, r_mut, bounds)
-				# store for next generation
-				children.append(c)
-		# replace population
-		pop = children
-	return [best, best_eval]
+    for _ in range(n_iter):
+        # evaluate all candidates in the population
+        scores = [objective(c) for c in pop]
+        # check for new best solution
+        for i in range(n_pop):
+            if scores[i] < best_eval:
+                best, best_eval = pop[i], scores[i]
+                #logger.info('>%d, new best f(%s) = %.3f' % (gen,  pop[i], scores[i]))
+        # select parents
+        selected = [selection(pop, scores) for _ in range(n_pop)]
+        # create the next generation
+        children = list()
+        for i in range(0, n_pop, 2):
+            # get selected parents in pairs
+            p1, p2 = selected[i], selected[i+1]
+            # crossover and mutation
+            for c in crossover(p1, p2, r_cross):
+                # mutation
+                mutation(c, r_mut, bounds)
+                # store for next generation
+                children.append(c)
+        # replace population
+        pop = children
+    return [best, best_eval]
