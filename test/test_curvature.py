b20d58b1495dbe7a11401f1bef47c7251b030fa0
Mário Antunes <mariolpantunes@gmail.com>
2021-09-09 10:35:38 +0100

    Update the curvature test to the new equation

--- test/test_curvature.py
+++ test/test_curvature.py
@@ -5,9 +5,9 @@ import numpy as np
 import knee.curvature as curvature
 
 
-class TestL_Method(unittest.TestCase):
+class TestCurvature(unittest.TestCase):
     def test_get_knee(self):
-        x = np.array([0,1,2,3,4,5,6,7,8,9,])
+        x = np.array([0,1,2,3,4,5,6,7,8,9])
         y = np.array([1,0.5,0.333333333,0.25,0.2,0.166666667,0.142857143,0.125,0.111111111,0.1])
         points = np.stack((x, y), axis=1)
         result = curvature.knee(points)
@@ -15,11 +15,11 @@ class TestL_Method(unittest.TestCase):
         self.assertEqual(result, desired)
     
     def test_multi_knee(self):
-        x = np.array([0,1,2,3,4,5,6,7,8,9,])
+        x = np.array([0,1,2,3,4,5,6,7,8,9])
         y = np.array([1,0.5,0.333333333,0.25,0.2,0.2,0.1,0.06666666667,0.05,0.04])
         points = np.stack((x, y), axis=1)
-        result = curvature.multi_knee(points, t1=0.9)
-        desired = np.array([1, 6])
+        result = curvature.multi_knee(points)
+        desired = np.array([1, 4, 5, 7])
         np.testing.assert_array_equal(result, desired)
 
 
--- test/test_lmethod.py
+++ test/test_lmethod.py
@@ -1,14 +1,21 @@
 import unittest
 import numpy as np
 
-
-from knee.lmethod import get_knee
+import knee.lmethod as lmethod
 
 
 class TestL_Method(unittest.TestCase):
     def test_get_knee_naive(self):
         x = np.array([0,1,2,3,4,5,6,7,8,9,])
         y = np.array([1,0.5,0.333333333,0.25,0.2,0.166666667,0.142857143,0.125,0.111111111,0.1])
-        result, _, _ = get_knee(x, y)
+        result, _, _ = lmethod.get_knee(x, y)
         desired = 2
         self.assertEqual(result, desired)
+    
+    def test_multi_knee(self):
+        x = np.array([0,1,2,3,4,5,6,7,8,9])
+        y = np.array([1,0.5,0.333333333,0.25,0.2,0.2,0.1,0.06666666667,0.05,0.04])
+        points = np.stack((x, y), axis=1)
+        result = lmethod.multi_knee(points)
+        desired = np.array([2, 5, 7])
+        np.testing.assert_array_equal(result, desired)
