3a01da2171f7fa7ba544b4ba842231267d4a5a61
Mário Antunes <mariolpantunes@gmail.com>
2021-10-12 10:20:28 +0100

    Added a flag to add_even_points function that allows the addition of the extreme points

--- test/test_postprocessing.py
+++ test/test_postprocessing.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import knee.rdp as rdp
 import knee.postprocessing as pp
 
 import logging
@@ -21,4 +22,24 @@ class TestPostProcessing(unittest.TestCase):
         knees = np.array([1,2,3])
         result = pp.filter_corner_knees(points, knees, .5)
         desired = np.array([1,3])
+        np.testing.assert_array_equal(result, desired)
+    
+    def test_add_even_points_0(self):
+        points = np.array([[0, 6], [1, 5], [2, 4], [3, 1], [4, 2], [5, 2], [6, 2], [7, 3], [8, 3], [9, 2], [10, 1], [11, 1/4], [12, 0]])
+        knees = np.array([1,2])
+
+        points_reduced, removed = rdp.rdp(points)
+
+        result = pp.add_points_even(points, points_reduced, knees, removed,  extremes=False)
+        desired = np.array([1,2,3,10,11,12])
+        np.testing.assert_array_equal(result, desired)
+    
+    def test_add_even_points_1(self):
+        points = np.array([[0, 6], [1, 5], [2, 4], [3, 1], [4, 2], [5, 2], [6, 2], [7, 3], [8, 3], [9, 2], [10, 1], [11, 1/4], [12, 0]])
+        knees = np.array([1,2])
+
+        points_reduced, removed = rdp.rdp(points)
+
+        result = pp.add_points_even(points, points_reduced, knees, removed,  extremes=True)
+        desired = np.array([0,1,2,3,10,11,12])
         np.testing.assert_array_equal(result, desired)
