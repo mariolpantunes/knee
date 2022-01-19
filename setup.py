812bf03012db08e7648b5883b433dcf84abe24d2
Mario Antunes <mariolpantunes@gmail.com>
2022-01-19 09:35:43 +0000

    Update requirements

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
       author_email='mariolpantunes@gmail.com',
       url='https://github.com/mariolpantunes/knee',
       packages=find_packages(),
-      install_requires=['numpy>=1.22.1', 'uts @ git+git://github.com/mariolpantunes/uts#egg=uts']
+      install_requires=['numpy>=1.22.1', 'uts @ git+git://github.com/mariolpantunes/uts@main#egg=uts']
 )
