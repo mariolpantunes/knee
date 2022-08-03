13cd4f24d81b30e608b6cbe206889511af824fc7
Mário Antunes <mariolpantunes@gmail.com>
2022-08-03 21:52:04 +0100

    Update RDP defaults to the one that perform better

--- knee/rdp.py
+++ knee/rdp.py
@@ -89,7 +89,7 @@ def compute_cost_coef(pt: np.ndarray, coef, cost: metrics.Metrics):
     return methods[cost](pt, coef)
 
 
-def rdp(points: np.ndarray, t: float = 0.01, cost: metrics.Metrics = metrics.Metrics.rpd, distance: Distance = Distance.shortest) -> tuple:
+def rdp(points: np.ndarray, t: float = 0.01, cost: metrics.Metrics = metrics.Metrics.smape, distance: Distance = Distance.shortest) -> tuple:
     """
     Ramer–Douglas–Peucker (RDP) algorithm.
 
@@ -201,7 +201,7 @@ def order_segment(pt: np.ndarray, index:int) -> tuple:
     return left_cost, right_cost
 
 
-def rdp_fixed(points: np.ndarray, length:int, distance: Distance = Distance.shortest, order:Order=Order.triangle) -> tuple:
+def rdp_fixed(points: np.ndarray, length:int, distance: Distance = Distance.shortest, order:Order=Order.segment) -> tuple:
     """
     Ramer–Douglas–Peucker (RDP) algorithm.
 
@@ -267,7 +267,7 @@ def rdp_fixed(points: np.ndarray, length:int, distance: Distance = Distance.shor
     return reduced, compute_removed_points(points, reduced)
 
 
-def grdp(points: np.ndarray, t: float = 0.01, cost: metrics.Metrics = metrics.Metrics.rpd, order:Order=Order.triangle, distance: Distance = Distance.shortest) -> tuple:
+def grdp(points: np.ndarray, t: float = 0.01, cost: metrics.Metrics = metrics.Metrics.smape, order:Order=Order.segment, distance: Distance = Distance.shortest) -> tuple:
     stack = [(0, 0, len(points))]
     reduced = [0, len(points)-1]
     
