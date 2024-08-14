f83ea7a7d6765d501e836b1fdd27fa773e6450f3
Mário Antunes <mario.antunes@ua.pt>
2024-08-14 23:00:44 +0100

    Update requirements.txt

--- reduce.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# load all the traces in the mrc folder
-for f in $(ls /home/mantunes/mrcs | egrep -i 'w[0-9]*-(lru|arc).csv' ); do
-    input='/home/mantunes/mrcs/'$f
-    output='/home/mantunes/mrcs/'${f%.*}'_reduced.csv'
-    #echo -e $input $output
-    python -m examples.reduce_trace -i $input -o $output
-done
